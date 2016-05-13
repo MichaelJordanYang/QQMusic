66872e817c5d7c59f0e01fa904d80c23d9f5e67e
jordanYang <897733537@qq.com>
2016-05-14 00:58:03 +0800

    界面搭建

---
+++ QQMusic/QQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQMusic
+//
+//  Created by xiaoyang on 16/5/14.
+//  Copyright © 2016年 yangChunFu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQMusic/QQMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQMusic
+//
+//  Created by xiaoyang on 16/5/14.
+//  Copyright © 2016年 yangChunFu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
